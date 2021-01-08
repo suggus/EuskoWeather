33162e676767d545009955ef0bbbffaf780e3f65
Gustavo Villa <gustavo.villa@ikasle.egibide.org>
2021-01-08 21:44:27 +0100

    Proyecto nuevo

---
+++ EuskoWeather/EuskoWeather/EuskoWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuskoWeatherApp.swift
+//  EuskoWeather
+//
+//  Created by  on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct EuskoWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
